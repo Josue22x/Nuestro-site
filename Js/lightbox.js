320a37de543080e48a23185bd0070d0fc9d425b6
Josue22x <145817136+Josue22x@users.noreply.github.com>
2023-12-10 20:50:43 -0600

    menu y promesa

--- Js/lightbox.js
+++ Js/lightbox.js
@@ -1,15 +1,21 @@
-const imagenes = document.querySelector('.img-galeria')
-const imagenesligt=document.querySelector('.agregar-imagen')
-const contenedorligt=document.querySelector('.imagen-ligt')
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesligt = document.querySelector('.agregar-imagen')
+const contenedorligt = document.querySelector('.imagen-ligt')
 
-imagenes.forEach(imagen => {
-    imagen.addEventListener('click', ()=>{
-        aparecerImagen(imagen.getAtribute('src'))
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click',()=>{
+        aparecerimagen( imagen.getAttribute('src'))
     })
-    
-});
+})
 
-const aparecerImagen=(imagen)=>{
+contenedorligt.addEventListener('click',(e)=>{
+    if(e.target !=imagenesligt){
+        contenedorligt.classList.toggle('show')
+        imagenesligt.classList.toggle('showImage')
+    }
+})
+
+const aparecerimagen =(imagen)=>{
     imagenesligt.src=imagen;
     contenedorligt.classList.toggle('show')
     imagenesligt.classList.toggle('showImage')
--- Js/menu.js
+++ Js/menu.js
@@ -1,9 +1,15 @@
 const menu = document.querySelector('.menu');
 const nave = document.querySelector('.menu-nav');
 
-console.log(menu)
-console.log(nave)
+
 
 menu.addEventListener('click', ()=>{
     nave.classList.toggle('spread')
+})
+
+window.addEventListener('click', e=>{
+    if(nave.classList.contains('spread')&& e.target != nave && e.target !=menu){
+        
+        nave.classList.toggle('spread')
+    }
 })
